a13e518f8e7eac755cc90c3909aa0fa265e12d74
ricardo-sanchez <ricardo.sanchez@globant.com>
2019-10-13 17:02:49 -0700

    Adding Eureka Server

--- src/main/java/com/globant/brainwaves/CoreEngineApplication.java
+++ src/main/java/com/globant/brainwaves/CoreEngineApplication.java
@@ -2,7 +2,11 @@ package com.globant.brainwaves;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
 public class CoreEngineApplication {
     public static void main(String[] args) {
